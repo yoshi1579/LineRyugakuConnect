527081dbc69851a04570bc540407c0a6f9e74a1a
yoshi1579 <vivayoshivitss1333@gmail.com>
2016-12-30 00:45:43 -0600

    Image slider compilete

---
+++ scripts/slider.js
@@ -0,0 +1,20 @@
+function Slider(){
+	var index = 1;
+	var $pointer = $(".slide-1");
+	$pointer.fadeIn(3000);
+	setInterval(function(){
+		$pointer.fadeOut(500, function()
+		{
+			$pointer.removeClass("slide-" + index);
+			index++;
+			if(index == 5)
+			{
+				index = 1;
+			}
+			$pointer.addClass("slide-" + index);
+			console.log("slide-" + index);
+		});
+		$pointer.fadeIn(1000);
+		$pointer.delay(1000);
+	}, 6500);
+}
