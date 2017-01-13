aa4442ff8c9893f5be3fb9091b71b1d95a51b3b3
yoshi1579 <vivayoshivitss1333@gmail.com>
2017-01-13 14:54:31 -0600

    add a lot of stuff

--- scripts/categoryScripts.js
+++ scripts/categoryScripts.js
@@ -1,9 +1,11 @@
 $(document).ready(function(){
 	setBindings();
+
+
 });
 
 function setBindings() {
-	$("nav a").click(function(e){
+	$("nav ul li a").click(function(e){
 		//e.preventDefault();
 		var sectionID = e.currentTarget.id + "Section";
 		if(sectionID != "noSection")
@@ -11,6 +13,49 @@ function setBindings() {
 			$("html body").animate({
 				scrollTop: $("#" + sectionID).offset().top
 			}, 1000);
+			$(this).parent().parent().toggleClass("open");
 		}
+	});
+
+	$(".dropDown img").click(function(){
+		
+		$(this).parent().next().toggleClass("open");
+
+		//close drop down menu when user click other place
+		$(document).click(function(event) { // (1)
+    	if (!$.contains($("nav")[0], event.target)) 
+    	{ // (2)
+       		 $("nav ul").removeClass("open");
+  	    }
+		});
+	});
+
+	// when the button is clicked, a modal window open
+	$("#modal-open").click(function(){
+
+		// preventing from opening multiple modal windows
+		$(this).blur();
+		if($("#modal-open")[0]) 
+		{
+			$("#modal-overlay").remove();
+		}
+
+		// generating overlay html code at the end of <body>
+		$("body").append('<div id="modal-overlay"></div>');
+
+		// fade in [modal-overlay]
+		$("#modal-overlay").fadeIn("slow");
+
+		//fade in [$modal-content]
+		$("#modal-content").fadeIn("slow");
+	});
+
+	// closing a modal window
+	$(document).on("click", "#modal-overlay, #modal-close", function(){
+		// fade out modal-overlay and modal-content
+		$("#modal-overlay, #modal-content").fadeOut("slow", function(){
+			// removing the html of modal-overlay after fading out
+			$("#modal-overlay").remove();
+		});
 	});
 }
