b6c05c933e0b4ae94c6ea285a341358a7f92fadc
yoshi1579 <vivayoshivitss1333@gmail.com>
2017-01-17 06:24:09 -0600

    Finished up the Line one

--- scripts/categoryScripts.js
+++ scripts/categoryScripts.js
@@ -30,12 +30,15 @@ function setBindings() {
 		});
 	});
 
+
+	// this variable decide which QR code to show
+	var qrcodeNum = 0;
 	// when the button is clicked, a modal window open
-	$("#modal-open").click(function(){
+	$(".modal-open").click(function(){
 
 		// preventing from opening multiple modal windows
 		$(this).blur();
-		if($("#modal-open")[0]) 
+		if($(".modal-open")[0]) 
 		{
 			$("#modal-overlay").remove();
 		}
@@ -47,15 +50,43 @@ function setBindings() {
 		$("#modal-overlay").fadeIn("slow");
 
 		//fade in [$modal-content]
-		$("#modal-content").fadeIn("slow");
+		$("#modal-content-" + qrcodeNum).fadeIn("slow");
 	});
 
 	// closing a modal window
 	$(document).on("click", "#modal-overlay, #modal-close", function(){
 		// fade out modal-overlay and modal-content
-		$("#modal-overlay, #modal-content").fadeOut("slow", function(){
+		$("#modal-content-" + qrcodeNum).fadeOut("slow");
+		$("#modal-overlay").fadeOut("slow", function(){
 			// removing the html of modal-overlay after fading out
 			$("#modal-overlay").remove();
+		});
+	});
+
+
+	// when a user click the next arrow, the content slider goes to next
+	$(".nextArrow").click(function(){
+		// increment qrcodeNum to trace which QR code to show
+		qrcodeNum++;
+
+		// remove open class from this row
+		$(this).parent().parent().parent().hide('slide',{direction:'left'},1000, function(){
+			$(this).next().hide();
+			// show the next content
+			$(this).next().fadeIn("slow");
+		});
+	});
+
+	// when a user click the back arrow, the content slider goes to previous one
+	$(".backArrow").click(function(){
+		// increment qrcodeNum to trace which QR code to show
+		qrcodeNum--;
+
+		// remove open class from this row
+		$(this).parent().parent().parent().hide('slide',{direction:'right'},1000, function(){
+			$(this).prev().hide();
+			// show the previous content
+			$(this).prev().fadeIn("slow");
 		});
 	});
 }
